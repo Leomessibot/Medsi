3f3900003bb1de302a72e681160afc758f08f990
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-02-13 14:54:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '8549422'))
-API_HASH = environ.get('API_HASH', '1cb02e0eb2c67d504e77c1bd268399ae')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6481155950:AAGpZao7ag3gQDiCnycnMR0Jfaxtnz2ps04')
+API_ID = int(environ.get('API_ID', '1522127'))
+API_HASH = environ.get('API_HASH', '1252ffe16baf341bfd7236f92df76b0e')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6821673610:AAGdw8ecQN5VXfiEDCAL1rw5IrOY6SK6EF8')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -22,29 +22,29 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/8619a6f258621134b7576.jpg https://telegra.ph/file/d8daf35960bbb4a7f8558.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6140023510').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002145296820').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1006159057 849970787').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
+auth_channel = "-1002024087674"
 auth_grp = environ.get('AUTH_GROUP')
-AUTH_CHANNEL = environ.get('AUTH_CHANNEL', '')
+AUTH_CHANNEL = "-1002024087674"
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-REQ_CHANNEL = environ.get("REQ_CHANNEL", '-1002145296820')
+REQ_CHANNEL = environ.get("REQ_CHANNEL", '-1002024087674')
 REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://F:F@cluster0.dlzznia.mongodb.net/")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Anurag_files')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://raja:AMRKS@cluster0.auwpwn0.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 pm = environ.get('PM')
 PM = int(pm) if pm and id_pattern.search(pm) else None
 
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001984264903'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002070840966'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TgX_Botz')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'True')), False)
 IMDB = is_enabled((environ.get('IMDB', 'False')), False)
