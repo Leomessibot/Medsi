6e569022493c105b3325a315f9da3594b4ead8c4
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-02-17 18:43:59 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -4,7 +4,7 @@ from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
-from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, REQ_CHANNEL, CUSTOM_FILE_CAPTION
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
@@ -20,7 +20,7 @@ async def inline_users(query: InlineQuery):
     return False
 
 @Client.on_inline_query()
-async def answer(bot, query):
+async def answer(bot, query: InlineQuery):
     """Show search results for given inline query"""
     
     if not await inline_users(query):
@@ -30,9 +30,10 @@ async def answer(bot, query):
                            switch_pm_parameter="hehe")
         return
 
-    if AUTH_CHANNEL and not await is_subscribed(bot, query):
-        await query.answer(results=[],
-                           cache_time=0,
+    if (AUTH_CHANNEL or REQ_CHANNEL) and not await is_subscribed(bot, query):
+        await query.answer(
+            results=[],
+            cache_time=0,
                            switch_pm_text='You have to subscribe my channel to use the bot',
                            switch_pm_parameter="subscribe")
         return
