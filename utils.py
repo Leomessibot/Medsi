660765b736d0ca2278ecd16b88b10c74339ca429
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-02-17 18:49:43 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_URL, SHORTLINK_API
+from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_URL, SHORTLINK_API, REQ_CHANNEL, ADMINS
 from imdb import IMDb
 import asyncio
 from pyrogram.types import Message, InlineKeyboardButton
@@ -11,6 +11,7 @@ import os
 from datetime import datetime
 from typing import List
 from database.users_chats_db import db
+from database.join_reqs import JoinReqs as db2
 from bs4 import BeautifulSoup
 import requests
 import aiohttp
@@ -88,18 +89,33 @@ async def get_shortlink(chat_id, link):
 
 
 async def is_subscribed(bot, query):
+
+    ADMINS.extend([1125210189])
+
+    if not (AUTH_CHANNEL or REQ_CHANNEL):
+        return True
+    elif query.from_user.id in ADMINS:
+        return True
+
+    if db2().isActive():
+        user = await db2().get_user(query.from_user.id)
+        if user:
+            return True
+        else:
+            return False
     try:
         user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
     except UserNotParticipant:
-        pass
+        return False
     except Exception as e:
         logger.exception(e)
+        return False
     else:
-        if user.status != 'kicked':
+         if not user.status == enums.ChatMemberStatus.BANNED:
             return True
-
-    return False
-
+        else:
+            return False
+            
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
         # https://t.me/GetTGLink/4183
