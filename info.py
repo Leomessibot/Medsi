6f28ce959b1210aa04a2a1c956885732a6df7c16
Mayavar258 <123068420+Mayavar258@users.noreply.github.com>
2024-02-18 10:53:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,10 +67,6 @@ MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
 
-#SHORTLINK_URL = environ.get('SHORTLINK_URL', 'onepagelink.in')
-#SHORTLINK_API = environ.get('SHORTLINK_API', 'd9021e1263d10c3a288f360f41c96e5426884193')
-
-
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
 LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
