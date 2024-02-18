3c422ea316bd83bc0bf9ce7b3f5924b432f5e107
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-02-18 13:28:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
-if [ -z $UPSTREAM_REPO ]
-then
+if [ -z "$UPSTREAM_REPO" ]; then
   echo "Cloning main Repository"
-  git clone https://github.com/Mayavar258/Flts.git /Flts
+  git clone -b notshort https://github.com/Mayavar258/Flts.git /Flts
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone -b notshort --single-branch "$UPSTREAM_REPO" /Flts
 fi
+
 cd /Flts
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
